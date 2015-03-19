bae28e1038f680bab068e715356094b33f2d5a7c
David Espí Hernández <davidespihernandez@gmail.com>
2015-03-19 11:52:34 +0100

    Graph working

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,4 +1,4 @@
-var market = angular.module('market', [])  //"highcharts-ng"
+var market = angular.module('market', ['chart.js'])  
 
 market.controller('FileListController', ['$scope', '$http', function($scope, $http){
     console.log("FileListController!");
@@ -63,106 +63,20 @@ market.controller('SearchController', ['$scope', '$http', function($scope, $http
     $scope.showSearching = false;
     $scope.dateFromInput = "";
     $scope.dateToInput = "";
-    $scope.locationInput = "";
+    $scope.locationInput = "AEC";
     $scope.nodeInput = "";
     $scope.dataList = [];
-    $scope.graphData = [];
-    //graph config object
-    $scope.highchartsNG = {
-        title: {
-            text: 'Monthly Average Temperature',
-            x: -20 //center
-        },
-        subtitle: {
-            text: 'Source: WorldClimate.com',
-            x: -20
-        },
-        xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        },
-        yAxis: {
-            title: {
-                text: 'Temperature (°C)'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
-        },
-        tooltip: {
-            valueSuffix: '°C'
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle',
-            borderWidth: 0
-        },
-        series: [{
-            name: 'Tokyo',
-            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-        }, {
-            name: 'New York',
-            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-        }, {
-            name: 'Berlin',
-            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-        }, {
-            name: 'London',
-            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-        }]
-    };
     
-    $scope.addPoints = function () {
-        var seriesArray = $scope.highchartsNG.series
-        var rndIdx = Math.floor(Math.random() * seriesArray.length);
-        seriesArray[rndIdx].data = seriesArray[rndIdx].data.concat([1, 10, 20])
+    $scope.labels = [" ", " "];
+    
+    $scope.series = ["LMP", "MLC", "MCC", "MEC"];
+    $scope.data = [
+        [0, 0],
+        [0, 0]
+    ];
+    $scope.onClick = function (points, evt) {
+        console.log(points, evt);
     };
-
-    $scope.addSeries = function () {
-        var rnd = []
-        for (var i = 0; i < 10; i++) {
-            rnd.push(Math.floor(Math.random() * 20) + 1)
-        }
-        $scope.highchartsNG.series.push({
-            data: rnd
-        })
-    }
-
-    $scope.removeRandomSeries = function () {
-        var seriesArray = $scope.highchartsNG.series
-        var rndIdx = Math.floor(Math.random() * seriesArray.length);
-        seriesArray.splice(rndIdx, 1)
-    }
-
-    $scope.options = {
-        type: 'line'
-    }
-
-    $scope.swapChartType = function () {
-        if (this.highchartsNG.options.chart.type === 'line') {
-            this.highchartsNG.options.chart.type = 'bar'
-        } else {
-            this.highchartsNG.options.chart.type = 'line'
-        }
-    }
-
-    $scope.highchartsNG = {
-        options: {
-            chart: {
-                type: 'bar'
-            }
-        },
-        series: [{
-            data: [10, 15, 12, 8, 7]
-        }],
-        title: {
-            text: 'Hello'
-        },
-        loading: false
-    }    
 
     $scope.search = function(){
         console.log("Searching ");
@@ -174,12 +88,27 @@ market.controller('SearchController', ['$scope', '$http', function($scope, $http
                 //console.log('Search response ' + response.toString());
                 $scope.showSearching = false;
                 $scope.dataList = response.detail;
-                //fill the graph
+                //fill the graph data
+                $scope.labels = [];
+                var LMP = [], MLC = [], MCC = [], MEC = [];
+                $scope.data = [];
+                $scope.dataList.forEach(function(item){
+                    $scope.labels.push(item.Interval);
+                    LMP.push(item.LMP);
+                    MLC.push(item.MLC);
+                    MCC.push(item.MCC);
+                    MEC.push(item.MEC);
+                });
+                $scope.data.push(LMP);
+                $scope.data.push(MLC);
+                $scope.data.push(MCC);
+                $scope.data.push(MEC);
             })
             .error(function(response){
                 console.log('Error searching!: ' + response);
             });
         
     };
+    $scope.search();
 }]);
 
